86da2ec70ff347f4f07e432e88abb538b1dc687d
Sealee <sealee02@gmail.com>
2019-03-13 21:40:21 +0800

    模型验证

---
+++ APIDemo/App_Start/MyActionFilterAttribute.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+using System.Web.Http.ModelBinding;
+
+namespace APIDemo.App_Start
+{
+    public class MyActionFilterAttribute: ActionFilterAttribute
+    {
+        public override Task OnActionExecutingAsync(HttpActionContext actionContext, CancellationToken cancellationToken)
+        {
+            if (!actionContext.ModelState.IsValid)
+            {
+                List<KeyValuePair<string, ModelState>> vs = actionContext.ModelState.ToList();
+                List<object> objList = new List<object>();
+                foreach (KeyValuePair<string, ModelState> item in vs)
+                {
+                    IList<string> strList = new List<string>();
+                    foreach (ModelError err in item.Value.Errors)
+                    {
+                        strList.Add(err.ErrorMessage);
+                    }
+                    objList.Add(new
+                    {
+                        key = item.Key.Split('.')[1],
+                        errorMessage = strList
+                    });
+                }
+                var obj = new
+                {
+                    errcode = -1,
+                    err = objList
+                };
+                actionContext.Response = new HttpResponseMessage()
+                {
+                    Content = new StringContent(JsonConvert.SerializeObject(obj), Encoding.UTF8, "application/json")
+
+                };
+            }
+
+            return base.OnActionExecutingAsync(actionContext, cancellationToken);
+        }
+    }
+}
---
+++ APIDemo/Controllers/ModelVerify/VerifyDemoController.cs
@@ -0,0 +1,31 @@
+﻿using APIDemo.App_Start;
+using APIDemo.Controllers.ModelVerify.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace APIDemo.Controllers.ModelVerify
+{
+
+    [MyActionFilter]
+    public class VerifyDemoController : ApiController
+    {
+
+        /// <summary>
+        /// 使用强类型接受，返回强类型     推荐这种，输入，输出都新建一个对应的Dto进行处理
+        /// </summary>
+        /// <param name="inDto">输入类</param>
+        /// <returns>输出类</returns>
+        [HttpPost]
+        public async Task<TestOutDto> TestAsync(TestInDto inDto)
+        {
+            TestOutDto outDto = new TestOutDto();
+            outDto.errcode = 0;
+            return await Task.FromResult(outDto);
+        }
+    }
+}
